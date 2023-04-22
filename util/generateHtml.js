c816caa8f8b4d2f3cdbace297915e8bb72169701
Alec Hunter <alecryanhunter@gmail.com>
2023-04-21 21:02:27 -0700

    edited html generation code for styling and html readability

--- util/generateHtml.js
+++ util/generateHtml.js
@@ -3,59 +3,59 @@ const generateTeam = team => {
 
     // create the manager html
     const generateManager = manager => {
-        return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
-        `;
+        return `                <div class="card employee-card m-3">
+                    <div class="card-header bg-primary text-light">
+                        <h2 class="card-title">${manager.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${manager.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                        </ul>
+                    </div>
+                </div>
+
+`;
     };
 
     // create the html for engineers
     const generateEngineer = engineer => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
+        return `                <div class="card employee-card m-3">
+                    <div class="card-header bg-primary text-light">
+                        <h2 class="card-title">${engineer.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                        </ul>
+                    </div>
+                </div>
+        
+`;
     };
 
     // create the html for interns
     const generateIntern = intern => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
-        `;
+        return `                <div class="card employee-card m-3">
+                    <div class="card-header bg-primary text-light">
+                        <h2 class="card-title">${intern.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                            <li class="list-group-item">School: ${intern.getSchool()}</li>
+                        </ul>
+                    </div>
+                </div>
+        
+`;
     };
 
     const html = [];
@@ -82,8 +82,7 @@ const generateTeam = team => {
 // export function to generate entire page
 module.exports = team => {
 
-    return `
-    <!DOCTYPE html>
+    return `<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -98,13 +97,15 @@ module.exports = team => {
 </head>
 
 <body>
+
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading bg-danger text-light">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
+
     <div class="container">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
@@ -112,7 +113,7 @@ module.exports = team => {
             </div>
         </div>
     </div>
+
 </body>
-</html>
-    `;
+</html>`;
 };
