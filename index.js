d3103d5d3fd753544e2fce72cd1bdc55e4eb7565
Alec Hunter <alecryanhunter@gmail.com>
2023-04-21 20:31:37 -0700

    added prompt structure. added extra view team option

--- index.js
+++ index.js
@@ -43,3 +43,147 @@
     // Finish
         // Build HTML
         // Exit
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const team = []
+
+async function init(){
+    console.log("Welcome to the Team Builder!")
+    
+    const manager = await managerMaker();
+    team.push(manager);
+    teamBuilder();
+}
+async function teamBuilder(){
+
+    const select = await inquirer.prompt([
+        {
+            name: "option",
+            type: "list",
+            choices: ["Add an Engineer","Add an Intern","View Current Team","Finish"],
+            message: "What would you like to do?",
+        }
+    ])
+
+    switch(select.option){
+        case "Add an Engineer":
+            const engineer = await engineerMaker();
+            team.push(engineer);
+            teamBuilder();
+            break;
+
+        case "Add an Intern":
+            const intern = await internMaker();
+            team.push(intern);
+            teamBuilder();
+            break;
+
+        case "View Current Team":
+            console.log(team);
+            await inquirer.prompt([
+                {
+                    name: "proceed",
+                    type: "input",
+                    message: "Press Enter to return.",
+                }
+            ])
+            teamBuilder();
+            break;
+
+        case "Finish":
+            console.log("Building Team...");
+            break;
+
+        default:
+            console.log("Something has gone wrong...");
+            break;
+    }
+    console.log("after switch function");
+}
+
+async function managerMaker(){
+    const manager = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please input the manager's name.",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: `Please input the manager's employee ID number.`,
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please input the manager's email address.",
+        },
+        {
+            name: "office",
+            type: "input",
+            message: "Please input the manager's office number.",
+        },
+    ])
+    const newManager = new Manager(manager.name,manager.id,manager.email,manager.office)
+    return newManager;
+}
+
+async function engineerMaker(){
+    const engineer = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please input the engineer's name.",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: `Please input the engineer's employee ID number.`,
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please input the engineer's email address.",
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "Please input the engineer's Github username.",
+        },
+    ])
+    const newEngineer = new Engineer(engineer.name,engineer.id,engineer.email,engineer.github)
+    return newEngineer;
+}
+
+async function internMaker(){
+    const intern = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please input the intern's name.",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: `Please input the intern's employee ID number.`,
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please input the intern's email address.",
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "Please input the intern's school.",
+        },
+    ])
+    const newIntern = new Intern(intern.name,intern.id,intern.email,intern.school)
+    return newIntern;
+}
+
+init();
